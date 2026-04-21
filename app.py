bde0903e18dd24fe1b0d39178d5daaf71a984517
Tomas <tomasmanrique200@gmail.com>
2026-04-21 16:52:58 +0000

    efficiency

--- app.py
+++ app.py
@@ -101,28 +101,24 @@ def cpu_widget():
     ).rename(columns={
         'cpu_avg_10s': 'cpu_usage'
     }).to_dict('records')
+            
+    cpu_df = (
+        df.loc[
+            (df['Tdie'].notna()) &
+            (df['Tdie'] > 0),
+            ['date', 'cpu_avg_10s', 'Tdie']
+        ]
+        .copy()
+        .sort_values('date')
+    )
 
-    cpu_data2 = []
-    for _, row in df.iterrows():
-        cpu_avg_trend = row['cpu_avg_10s']
-        tdie_trend = row['Tdie']
-        if pd.notna(tdie_trend) and tdie_trend > 0:
-            cpu_data2.append({
-                    "date": str(row['date'].strftime('%d %H:%M')),
-                    "cpu_usage": cpu_avg_trend,
-                    "Tdie": tdie_trend
-                })
-
-    labels = [d['date'] for d in cpu_data][::2]
-    cpu_usage_trend = [d['cpu_usage_3_avg'] for d in cpu_data][::2]
-    tdie_trend = [d['Tdie_3_avg'] for d in cpu_data][::2]
-
-    # Convert the list of dictionaries to a DataFrame for easier manipulation
-    cpu_df = pd.DataFrame(cpu_data2)
+    labels = [d['date'] for d in cpu_data][::3]
+    cpu_usage_trend = [d['cpu_usage_3_avg'] for d in cpu_data][::3]
+    tdie_trend = [d['Tdie_3_avg'] for d in cpu_data][::3]
 
     # Extract the maximum CPU usage from the 'cpu_usage' column
-    max_cpu_usage = cpu_df['cpu_usage'].max()
-    percentile_95_cpu_usage = round(cpu_df['cpu_usage'].quantile(0.95),1)
+    max_cpu_usage = cpu_df['cpu_avg_10s'].max()
+    percentile_95_cpu_usage = round(cpu_df['cpu_avg_10s'].quantile(0.95),1)
     max_tdie = cpu_df['Tdie'].max()
     min_tdie = cpu_df['Tdie'].min()
     percentile_95_tdie = round(cpu_df['Tdie'].quantile(0.95),1)
@@ -340,9 +336,21 @@ def ups_widget():
         date=ups_data['date'].dt.strftime('%d %H:%M')
     ).to_dict('records')
 
-    labels = [d['date'] for d in ups_data][::2]
-    battery_charge_trend = [d['battery_charge'] for d in ups_data][::2]
-    power_trend = [d['power_3_avg'] for d in ups_data][::2]
+    ups_data2 = (
+        df.loc[
+            (df['power'].notna()) &
+            (df['power'] > 0),
+            ['date', 'power']
+        ]
+        .copy()
+        .sort_values('date')
+    )
+
+    labels = [d['date'] for d in ups_data][::3]
+    battery_charge_trend = [d['battery_charge'] for d in ups_data][::3]
+    power_trend = [d['power_3_avg'] for d in ups_data][::3]
+
+    avg_power = round(ups_data2['power'].mean(), 1)
 
     html = f"""
 <!DOCTYPE html>
@@ -378,7 +386,7 @@ def ups_widget():
   <div style="background: rgba(37,40,60,1); border: 1px solid hsl(232, 23%, 22%); box-shadow: 0px 3px 0px 0px hsl(232, 23%, 18%); border-radius: 8px; padding: 16px 16px;">
   <p><span style="color: #a0b4d0">Status </span><span class="status" style="font-weight:500;">{status_label}</span></p>
   <p><span style="color: #a0b4d0">Runtime </span><span style="font-weight:500; color: #60A2F4">{battery_runtime} min</span><span style="color: #a0b4d0"> // Low Alarm </span><span style="font-weight:500; color: #F46060">{battery_runtime_low} min</span></p>
-  <p><span style="color: #a0b4d0">Current Power Consumption: </span><span style="font-weight:500; color: #A070FF">{load_watts} W</span></p>
+  <p><span style="color: #a0b4d0">Current Power: </span><span style="font-weight:500; color: #A070FF">{load_watts} W</span><span style="color: #a0b4d0"> // Avg power (last 7d): </span><span style="font-weight:500; color: #A070FF">{avg_power} W</span></p>
   <p><span style="color: #a0b4d0">Output Voltage </span><span style="font-weight:500; color: #60A2F4">{output_voltage} V</span></p>
   <div style="margin-top: 10px;">
   <p class="hl" style="margin-top:8px; font-weight:500; margin:4px 0 0; color:#ffffff">Trends - Last 24h</p>
@@ -409,7 +417,7 @@ Chart.defaults.font.family = 'Inter, sans-serif';
     data: {{
       labels: {labels},
       datasets: [
-        {{ label: 'Batery Charge (%)', data: {battery_charge_trend}, pointStyle: 'circle', backgroundColor: '#60f4a2', borderColor: '#60f4a2', borderWidth: 2.5, fill:  false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#60f4a2', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5 }},
+        {{ label: 'Batery Charge (%)', data: {battery_charge_trend}, pointStyle: 'circle', backgroundColor: '#60f4a2', borderColor: '#60f4a2', borderWidth: 1.2, fill:  false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#60f4a2', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5 }},
         {{ label: 'Power (W)', data: {power_trend}, pointStyle: 'circle', backgroundColor: '#a070ff', borderColor: '#a070ff', borderWidth: 1.2, fill: false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#a070ff', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5, yAxisID: 'y2' }}
       ]
     }},
@@ -541,27 +549,23 @@ def mem_disk_widget():
         'mem_pct': 'mem_usage', 'zfs_tank_hdd_pct': 'tank_hdd_usage'
     }).to_dict('records')
 
-    mem_disk_data2 = []
-    for _, row in df.iterrows():
-        mem_trend = row['mem_pct']
-        disc_tank_hdd_trend = row['zfs_tank_hdd_pct']
-        if pd.notna(mem_trend) and mem_trend > 0:
-            mem_disk_data2.append({
-                    "date": str(row['date'].strftime('%d %H:%M')),
-                    "mem_usage": mem_trend,
-                    "tank_hdd_usage": disc_tank_hdd_trend
-                })
+    mem_df = (
+        df.loc[
+            (df['mem_pct'].notna()) &
+            (df['mem_pct'] > 0),
+            ['date', 'mem_pct', 'zfs_tank_hdd_pct']
+        ]
+        .copy()
+        .sort_values('date')
+    )
 
     labels = [d['date'] for d in mem_disk_data][::5]
     mem_usage_trend = [d['mem_usage'] for d in mem_disk_data][::5]
     tank_hdd_trend = [d['tank_hdd_usage'] for d in mem_disk_data][::5]
 
-    # Convert the list of dictionaries to a DataFrame for easier manipulation
-    mem_df = pd.DataFrame(mem_disk_data2)
-
     # Extract the maximum RAM usage from the 'mem_usage' column
-    max_mem_usage = mem_df['mem_usage'].max()
-    max_zfs_usage = round(mem_df['tank_hdd_usage'].max() * 3.35 * 0.01, 1)
+    max_mem_usage = mem_df['mem_pct'].max()
+    max_zfs_usage = round(mem_df['zfs_tank_hdd_pct'].max() * 3.35 * 0.01, 1)
 
     html = f"""
 <!DOCTYPE html>
@@ -586,7 +590,7 @@ def mem_disk_widget():
 </head>
 <body>  
   <div style="background: rgba(37,40,60,1); border: 1px solid hsl(232, 23%, 22%); box-shadow: 0px 3px 0px 0px hsl(232, 23%, 18%); border-radius: 8px; padding: 16px 16px;">
-  <p><span style="color: #a0b4d0">Max RAM usage (last 7d): </span><span style="font-weight:500; color: #A070FF">{max_mem_usage} min</span></p>
+  <p><span style="color: #a0b4d0">Max RAM usage (last 7d): </span><span style="font-weight:500; color: #A070FF">{max_mem_usage} %</span></p>
   <p><span style="color: #a0b4d0">Max ZFS used capacity (last 7d): </span><span style="font-weight:500; color: #60F4A2">{max_zfs_usage} TB</span><span style="color: #a0b4d0"> out of </span><span style="font-weight:500; color: #60F4A2">3.35 TB</span></p>
   <div style="margin-top: 10px;">
   <p class="hl" style="margin-top:8px; font-weight:500; margin:4px 0 0; color:#ffffff">Trends - Last 48h</p>
@@ -617,8 +621,8 @@ Chart.defaults.font.family = 'Inter, sans-serif';
     data: {{
       labels: {labels},
       datasets: [
-        {{ label: 'RAM usage (%)', data: {mem_usage_trend}, pointStyle: 'circle', backgroundColor: '#a070ff', borderColor: '#a070ff', borderWidth: 2.5, fill:  false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#a070ff', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5 }},
-        {{ label: 'ZFS Tank-hdd usage (%)', data: {tank_hdd_trend}, pointStyle: 'circle', backgroundColor: '#60f4a2', borderColor: '#60f4a2', borderWidth: 2.5, fill:  false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#60f4a2', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5 }},
+        {{ label: 'RAM usage (%)', data: {mem_usage_trend}, pointStyle: 'circle', backgroundColor: '#a070ff', borderColor: '#a070ff', borderWidth: 1.2, fill:  false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#a070ff', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5 }},
+        {{ label: 'ZFS Tank-hdd usage (%)', data: {tank_hdd_trend}, pointStyle: 'circle', backgroundColor: '#60f4a2', borderColor: '#60f4a2', borderWidth: 1.2, fill:  false, pointRadius: 0, pointHoverRadius: 6, pointHoverBackgroundColor: '#60f4a2', pointHoverBorderWidth: 2, pointHoverBorderColor: '#ffffff', tension: 0.5 }},
       ]
     }},
     options: {{
